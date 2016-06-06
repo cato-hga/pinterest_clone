1d3332c2e53f9e57c880468c08a14e1c3269caa3
cato-hga <jchristopher.cato@gmail.com>
2016-06-05 23:36:37 -0400

    Pins CRUD functionality

---
+++ app/models/pin.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: pins
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Pin < ActiveRecord::Base
+end
