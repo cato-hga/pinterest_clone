f156d6e9f397be474f7bb6097682dc22ef4a6e7e
cato-hga <jchristopher.cato@gmail.com>
2016-06-06 01:11:23 -0400

    Add Devise and User association

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,7 +7,9 @@
 #  description :text
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :integer
 #
 
 class Pin < ActiveRecord::Base
+  belongs_to :user
 end
